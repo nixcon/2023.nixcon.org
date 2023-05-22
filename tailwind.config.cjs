745bf99d5290446d751837ac16b7e4d610adddda
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-05-22 22:17:56 +0200

    first steps and gitignore now ignores result

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        nixblue: {
+          DEFAULT: "#6586C8",
+          50: "#ECF0F8",
+          100: "#DDE4F3",
+          200: "#BFCDE8",
+          300: "#A1B5DD",
+          400: "#839ED3",
+          500: "#6586C8",
+          600: "#4067B4",
+          700: "#324F8B",
+          800: "#233862",
+          900: "#142038",
+          950: "#0D1424",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
