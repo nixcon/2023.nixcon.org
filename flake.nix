1f8dc543371d407bd15faa0535920f7e2eb5f917
Andreas Rammhold <andreas@rammhold.de>
2023-05-29 20:54:02 +0200

    fix nix build

--- flake.nix
+++ flake.nix
@@ -1,26 +1,38 @@
 {
   # Override nixpkgs to use the latest set of node packages
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.npmlock2nix = {
+    url = "github:nix-community/npmlock2nix";
+    flake = false;
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    npmlock2nix
   }:
     flake-utils.lib.eachDefaultSystem
     (system: let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [
+          (self: super: {
+            npmlock2nix = super.callPackage npmlock2nix { };
+          })
+        ];
       };
     in {
-      packages.default = pkgs.buildNpmPackage {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.nodejs ];
+      };
+      packages.default = pkgs.npmlock2nix.v2.build {
+        nodejs = pkgs.nodejs;
         name = "nixcon-2023";
         src = ./.;
-        npmDepsHash = "sha256-KegUJItyeaevVtnUR2sheuu1C8XqGWLy9t9oP4O+cis=";
-        installPhase = ''
-          mkdir -p $out
-          cp -a ./dist/. $out
-        '';
+
+        preBuild = "export HOME=$(pwd)";
+        installPhase = "cp -rv dist $out";
       };
     });
 }
