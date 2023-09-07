5ef0d70f4c3826a2385ab198d272c1cd2db79aa2
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-09-07 15:32:30 +0200

    add svelte, make sponsors sort randomly by hydration

---
+++ src/components/common/Sponsors/Sponsors.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Sponsor from "./Sponsor.svelte";
+
+  export let sponsors,
+    level,
+    randomize = true;
+
+  const randomizedSponsors = randomize
+    ? sponsors
+        .map((sponsor) => ({ sponsor, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ sponsor }) => sponsor)
+    : sponsors;
+</script>
+
+{#each randomizedSponsors as sponsor}
+    <Sponsor {level} image={sponsor.image} alt={sponsor.alt} url={sponsor.url} extraClasses={sponsor.extraClasses}/>
+{/each}
